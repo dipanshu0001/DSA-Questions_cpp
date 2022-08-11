a8fae464cba37d231ab3dd119268cdefc43e9434
dipanshu0001 <dipanshugupta1624@gmail.com>
2022-08-11 23:56:57 +0530

    11/8/2022

--- gfg/nine_divsor_9_8.cpp
+++ gfg/nine_divsor_9_8.cpp
@@ -27,6 +27,29 @@ public:
 
 
     ///////////// below approach takes o(sqrt(N)) space and time
+    /* 
+    here we have taken sieve of size sqrt(n) as maximum of that divsor can be 
+    possible for n after that if we even make it will of no use as example 100 will 
+    have max divisors upto 10 only if we take even 11 it will cross the value 100 
+    so by seive we find all the prime element upto sqrt(n) an elment which are multiple of prime we stroe thet firsttime which prime number reached their
+    now a number have atleast have one divsor which will be prime
+    so for each number while taking its  ex 36=>2,2,3,3
+    so 2^2 and 3^2 here (2+1)*(2+1) this much divisor 36 will have
+    now we e=wanrt 9 divisor so p^2^*q^2=n
+    here p and q cant be equal as it will go greater than n and also q cant be 1 
+    for ex if we get p^4 then we get 5 divisor but we will get 6 as here q will bt 1 having power 0 which will contrubute 1 from their
+    corner case is that if p^8 is their than it is possible to have 9 divisor
+    to check 3 conditon p*q!=n and q!=1 and p!=q
+    else if chech is that is originally prime by mathcing it with its index if true then check if pow raise to 8 
+    that index is less than qeual to n if true than cnt++
+
+    
+    
+    
+    
+    
+    
+    */
     long long int nineDivisors(long long int N){
         long long int size=sqrt(N);
         vector<long long int> v((int)size+1);
